8550aef3998f107b9abb37653bebdfee1ed2ba37
Michael Scofield <mscofield0@tutanota.com>
2025-03-05 22:23:53 +0100

    add a timestamp to the events

--- src/res.rs
+++ src/res.rs
@@ -8,17 +8,7 @@ pub struct Res<E> {
 	/// The targets that this event is sent to.
 	pub targets: Targets,
 	/// The event that occurred.
-	pub event: E,
-}
-
-impl<E> Res<E> {
-	/// Creates a new [`Res`] event.
-	pub fn new(targets: impl Into<Targets>, event: impl Into<E>) -> Self {
-		Self {
-			targets: targets.into(),
-			event: event.into(),
-		}
-	}
+	pub event: TimestampedEvent<E>,
 }
 
 impl<E> PartialEq for Res<E>
@@ -56,5 +46,58 @@ where
 impl<E> Into<Vec<Res<E>>> for Res<E> {
 	fn into(self) -> Vec<Res<E>> {
 		vec![self]
+	}
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct TimestampedEvent<E> {
+	/// The timestamp of when the event occurred (in ms).
+	pub timestamp: i64,
+	/// The event that occurred.
+	pub event: E,
+}
+
+impl<E> TimestampedEvent<E> {
+	/// Creates a new event.
+	pub fn new(event: impl Into<E>) -> Self {
+		Self {
+			timestamp: chrono::Utc::now().timestamp_millis(),
+			event: event.into(),
+		}
+	}
+}
+
+impl<E> PartialEq for TimestampedEvent<E>
+where
+	E: PartialEq,
+{
+	fn eq(&self, other: &Self) -> bool {
+		self.event == other.event
+	}
+}
+
+impl<E> Eq for TimestampedEvent<E> where E: Eq {}
+
+impl<E> std::fmt::Debug for TimestampedEvent<E>
+where
+	E: std::fmt::Debug,
+{
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		f.debug_struct("TimestampedEvent")
+			.field("timestamp", &self.timestamp)
+			.field("event", &self.event)
+			.finish()
+	}
+}
+
+impl<E> Clone for TimestampedEvent<E>
+where
+	E: Clone,
+{
+	fn clone(&self) -> Self {
+		Self {
+			timestamp: self.timestamp.clone(),
+			event: self.event.clone(),
+		}
 	}
 }
